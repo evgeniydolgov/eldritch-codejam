4260bf2d93a4beb188f6c94ad2964f2378c380b3
evgeniydolgov <amyr214519@gmail.com>
2022-08-26 12:28:12 +0300

    init: created eldritch-codejam task

--- data/mythicCards/green/index.js
+++ data/mythicCards/green/index.js
@@ -1,114 +1,112 @@
-import greenCardsAssets from '../../../assets/MythicCards/green';
-
-const cardsData = [
+const cardsDataGreen = [
   {
     id: 'green1',
-    cardFace: greenCardsAssets.green1,
+    cardFace: './assets/MythicCards/green1.png',
     difficulty: 'easy',
     color:'green'
   },
   {
     id: 'green2',
-    cardFace: greenCardsAssets.green2,
+    cardFace: './assets/MythicCards/green2.png',
     difficulty: 'hard',
     color:'green'
   },
   {
     id: 'green3',
-    cardFace: greenCardsAssets.green3,
+    cardFace: './assets/MythicCards/green3.png',
     difficulty: 'hard',
     color:'green'
   },
   {
     id: 'green4',
-    cardFace: greenCardsAssets.green4,
+    cardFace: './assets/MythicCards/green4.png',
     difficulty: 'hard',
     color:'green'
   },
   {
     id: 'green5',
-    cardFace: greenCardsAssets.green5,
+    cardFace: './assets/MythicCards/green5.png',
     difficulty: 'hard',
     color:'green'
   },
   {
     id: 'green6',
-    cardFace: greenCardsAssets.green6,
+    cardFace: './assets/MythicCards/green6.png',
     difficulty: 'hard',
     color:'green'
   },
   {
     id: 'green7',
-    cardFace: greenCardsAssets.green7,
+    cardFace: './assets/MythicCards/green7.png',
     difficulty: 'normal',
     color:'green'
   },
   {
     id: 'green8',
-    cardFace: greenCardsAssets.green8,
+    cardFace: './assets/MythicCards/green8.png',
     difficulty: 'normal',
     color:'green'
   },
   {
     id: 'green9',
-    cardFace: greenCardsAssets.green9,
+    cardFace: './assets/MythicCards/green9.png',
     difficulty: 'normal',
     color:'green'
   },
   {
     id: 'green10',
-    cardFace: greenCardsAssets.green10,
+    cardFace: './assets/MythicCards/green10.png',
     difficulty: 'normal',
     color:'green'
   },
   {
     id: 'green11',
-    cardFace: greenCardsAssets.green11,
+    cardFace: './assets/MythicCards/green11.png',
     difficulty: 'normal',
     color:'green'
   },
   {
     id: 'green12',
-    cardFace: greenCardsAssets.green12,
+    cardFace: './assets/MythicCards/green12.png',
     difficulty: 'easy',
     color:'green'
   },
   {
     id: 'green13',
-    cardFace: greenCardsAssets.green13,
+    cardFace: './assets/MythicCards/green13.png',
     difficulty: 'normal',
     color:'green'
   },
   {
     id: 'green14',
-    cardFace: greenCardsAssets.green14,
+    cardFace: './assets/MythicCards/green14.png',
     difficulty: 'normal',
     color:'green'
   },
   {
     id: 'green15',
-    cardFace: greenCardsAssets.green15,
+    cardFace: './assets/MythicCards/green15.png',
     difficulty: 'normal',
     color:'green'
   },
   {
     id: 'green16',
-    cardFace: greenCardsAssets.green16,
+    cardFace: './assets/MythicCards/green16.png',
     difficulty: 'easy',
     color:'green'
   },
   {
     id: 'green17',
-    cardFace: greenCardsAssets.green17,
+    cardFace: './assets/MythicCards/green17.png',
     difficulty: 'easy',
     color:'green'
   },
   {
     id: 'green18',
-    cardFace: greenCardsAssets.green18,
+    cardFace: './assets/MythicCards/green18.png',
     difficulty: 'easy',
     color:'green'
   },
 ]
 
-export default cardsData
+export default cardsDataGreen
